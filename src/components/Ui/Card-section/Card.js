4edf9acba5fb867944ef2f7a32d010038c3076f1
jennifer-pecky <ugwojennifer7@gmail.com>
2023-01-23 11:46:39 -0800

    still working

--- src/components/Ui/Card-section/Card.js
+++ src/components/Ui/Card-section/Card.js
@@ -6,10 +6,13 @@ import img from "../../../Asset/images/png.png"
 const Card = () => {
     const { globalPadding } = styles
     return (
-        <section className={`${globalPadding}`}>
+        <section className={`${globalPadding} px-20`}>
             <div className='flex justify-between'>
-                <h1 className=''>Featured Listings</h1>
-                <h3>VIEW MORE</h3>
+                <h1 className='font-bold text-4xl'>Featured Listings</h1>
+                <a href="#" className='text-[#EC522E] mt-1 -space-y-8'>
+                    VIEW MORE
+                    <hr class="w-[5.4rem] h-[0.1rem] rounded dark:bg-[#EC522E]"></hr>
+                </a>
             </div>
 
             <div className=''>
