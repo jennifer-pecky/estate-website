3e1276dbc3a9eb21cb31d561387efe980a47edcd
jennifer-pecky <ugwojennifer7@gmail.com>
2023-01-24 07:37:14 -0800

    compeleted

---
+++ src/Asset/Data/Data.js
@@ -0,0 +1,31 @@
+import img1 from '../images/Image.png'
+import img2 from '../images/png.png'
+import img3 from '../images/Rectangle 2.png'
+
+
+
+export const CARD__DATA = [
+    {
+        img: "img1",
+        title: "4 Bedroom Duplex at Odili Road",
+        prize: "N50,000,000",
+        desc: "Odili Road, Port Harcourt,Rivers State"
+
+    },
+
+    {
+        img: "img1",
+        title: "4 Bedroom Duplex at Odili Road",
+        prize: "N50,000,000",
+        desc: "Odili Road, Port Harcourt,Rivers State"
+
+    },
+
+    {
+        img: "img1",
+        title: "4 Bedroom Duplex at Odili Road",
+        prize: "N50,000,000",
+        desc: "Odili Road, Port Harcourt,Rivers State"
+
+    },
+]
--- src/components/Ui/Card-section/Card.js
+++ src/components/Ui/Card-section/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { styles } from '../../../styles/styles.tailwind'
-import img from "../../../Asset/images/png.png"
+import img from "../../../Asset/images/png2.png"
 
 
 const Card = () => {
@@ -15,8 +15,8 @@ const Card = () => {
                 </a>
             </div>
 
-            <div className=''>
-                <img src={img} alt="" />
+            <div className='flex flex-col mt-10 w-[370px] h-[631px] border-2 border-[#707070]'>
+                <img src={img} alt="" className='w-[370px] h-[340px]' />
                 <div className='px-5'>
                     <p className='font-bold text-base tracking-wide'>4 Bedroom Duplex at Odili Road</p>
                     <span className='text-[#EC522E] font-bold tracking-wide'>N50,000,000</span>
@@ -27,11 +27,10 @@ const Card = () => {
                         <button className='tracking-wide rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Bar spot</button> <br />
                     </div>
                     <button className='mt-3 tracking-wide rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Swimming pool</button>
-                </div>
-
-                <div className='flex space-x-3 px-6 mt-3'>
-                    <button className='text-sm mt-3 font-semibold rounded-[6px] w-[129px] h-[42px] text-[#EC522E] border-2 border-[#EC522E]'>VIEW</button>
-                    <button className='text-sm mt-3 font-semibold rounded-[6px] w-[185px] h-[42px] text-[#fff] border-2 border-[#EC522E] bg-[#EC522E]'>CONTACT</button>
+                    <div className='flex space-x-3 mt-2'>
+                        <button className='text-sm mt-3 font-semibold rounded-[6px] w-[129px] h-[42px] text-[#EC522E] border-2 border-[#EC522E]'>VIEW</button>
+                        <button className='text-sm mt-3 font-semibold rounded-[6px] w-[185px] h-[42px] text-[#fff] border-2 border-[#EC522E] bg-[#EC522E]'>CONTACT</button>
+                    </div>
                 </div>
             </div>
         </section>
