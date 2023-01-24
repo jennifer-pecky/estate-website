09aac46e0ef29921959e4c5d80ca5fc26ff8ecc9
jennifer-pecky <ugwojennifer7@gmail.com>
2023-01-23 22:53:04 -0800

    about to start props

--- src/components/Ui/Card-section/Card.js
+++ src/components/Ui/Card-section/Card.js
@@ -19,19 +19,19 @@ const Card = () => {
                 <img src={img} alt="" />
                 <div className='px-5'>
                     <p className='font-bold text-base tracking-wide'>4 Bedroom Duplex at Odili Road</p>
-                    <span className='text-[#EC522E] font-bold'>N50,000,000</span>
-                    <p className='font-medium'>Odili Road, Port Harcourt,Rivers State</p>
+                    <span className='text-[#EC522E] font-bold tracking-wide'>N50,000,000</span>
+                    <p className='font-medium tracking-wide'>Odili Road, Port Harcourt,Rivers State</p>
                     <div className='flex space-x-3'>
-                        <button className='rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Watered</button>
-                        <button className='rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Car pack</button>
-                        <button className='rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Bar spot</button> <br />
+                        <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Watered</button>
+                        <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Car pack</button>
+                        <button className='tracking-wide rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Bar spot</button> <br />
                     </div>
-                    <button className='mt-3 rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Swimming pool</button>
+                    <button className='mt-3 tracking-wide rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Swimming pool</button>
                 </div>
 
-                <div className='px-5'>
-                    <button>VIEW</button>
-                    <button>CONTACT</button>
+                <div className='flex space-x-3 px-6 mt-3'>
+                    <button className='text-sm mt-3 font-semibold rounded-[6px] w-[129px] h-[42px] text-[#EC522E] border-2 border-[#EC522E]'>VIEW</button>
+                    <button className='text-sm mt-3 font-semibold rounded-[6px] w-[185px] h-[42px] text-[#fff] border-2 border-[#EC522E] bg-[#EC522E]'>CONTACT</button>
                 </div>
             </div>
         </section>
