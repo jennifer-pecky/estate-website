190ba2b9d5b774e559ed5638ebf07babbda6414d
jennifer-pecky <ugwojennifer7@gmail.com>
2023-01-24 09:43:33 -0800

    made changes

--- src/components/Ui/Card-section/Card.js
+++ src/components/Ui/Card-section/Card.js
@@ -15,21 +15,21 @@ const Card = () => {
                 </a>
             </div>
 
-            <div className='flex flex-col mt-10 w-[370px] h-[631px] border-2 border-[#707070]'>
+            <div className='flex flex-col mt-10 mb-9 w-[370px] h-[631px] border-2 border-[#707070] rounded-[8px] space-y-2'>
                 <img src={img} alt="" className='w-[370px] h-[340px]' />
-                <div className='px-5'>
+                <div className='px-5 space-y-1'>
                     <p className='font-bold text-base tracking-wide'>4 Bedroom Duplex at Odili Road</p>
-                    <span className='text-[#EC522E] font-bold tracking-wide'>N50,000,000</span>
+                    <p className='text-[#EC522E] font-bold tracking-wide'>N50,000,000</p>
                     <p className='font-medium tracking-wide'>Odili Road, Port Harcourt,Rivers State</p>
                     <div className='flex space-x-3'>
-                        <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Watered</button>
-                        <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Car pack</button>
-                        <button className='tracking-wide rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Bar spot</button> <br />
+                        <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] p-2 px-4 py-2'>Watered</button>
+                        <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] p-2 px-4 py-2'>Car pack</button>
+                        <button className='mt-1 tracking-wide rounded-full text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] p-2 px-4 py-2'>Bar spot</button> <br />
                     </div>
-                    <button className='mt-3 tracking-wide rounded-full text-xs border-[1px] font-semibold border-[#707070] p-2 px-4 py-2'>Swimming pool</button>
-                    <div className='flex space-x-3 mt-2'>
-                        <button className='text-sm mt-3 font-semibold rounded-[6px] w-[129px] h-[42px] text-[#EC522E] border-2 border-[#EC522E]'>VIEW</button>
-                        <button className='text-sm mt-3 font-semibold rounded-[6px] w-[185px] h-[42px] text-[#fff] border-2 border-[#EC522E] bg-[#EC522E]'>CONTACT</button>
+                    <button className='mt-3 tracking-wide rounded-full text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] p-2 px-4 py-2'>Swimming pool</button>
+                    <div className='flex space-x-3'>
+                        <button className='text-sm mt-5 font-semibold rounded-[6px] w-[129px] h-[42px] hover:bg-[#707070] text-[#EC522E] border-2 border-[#EC522E]'>VIEW</button>
+                        <button className='text-sm mt-5 font-semibold rounded-[6px] w-[185px] h-[42px] hover:bg-[#707070] text-[#fff] border-2 border-[#EC522E] bg-[#EC522E]'>CONTACT</button>
                     </div>
                 </div>
             </div>
