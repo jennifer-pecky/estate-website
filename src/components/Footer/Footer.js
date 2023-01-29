436222311f0554f6bdd6c0522075d4f30edb126a
jennifer-pecky <ugwojennifer7@gmail.com>
2023-01-29 06:34:58 -0800

    completed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { styles } from '../../styles/styles.tailwind'
 
+
 const Footer = () => {
     const { globalPadding } = styles
     return (
@@ -15,33 +16,40 @@ const Footer = () => {
                 </div>
 
                 <div className='flex relative px-10'>
-                    <div className='flex w-[690px] h-[400px] bg-[#fff]  absolute bottom-6 '>
-                        <form action="" className='space-y-5 p-10'>
-                            <input className='p-6 w-[289px] h-4 border-[0.5px] border-[#707070] rounded-[8px]' type="text" placeholder='Your Name' />
-                            <input className='p-6 w-[289px] h-4 border-[0.5px] border-[#707070] rounded-[8px]' type="text" placeholder='Your Email' />
-                            <input className='p-8 w-[570px] h-[166px] border-[1px]  border-[#707070]  rounded-[8px]' type="text" placeholder='Your Message' />
-                        </form>
-                    </div>
+                    <div className='flex w-[690px]  bg-[#fff]  absolute bottom-6'>
+                        <div className='p-10'>
+                            <form action="/action_page-php" className='space-y-5'>
+                                <input className='p-6 w-[289px] h-4 border-[0.5px] border-[#707070] rounded-[8px]' type="text" placeholder='Your Name' />
+                                <input className='p-6 w-[289px] h-4 border-[0.5px] border-[#707070] rounded-[8px]' type="text" placeholder='Your Email' />
+                                <input className='p-8 w-[570px] h-[166px] border-[1px]  border-[#707070]  rounded-[8px]' type="text" placeholder='Your Message' />
+                            </form>
+                            <button className='w-[154px] h-[61px] ml-[69%] mt-4 hover:bg-[#666666] rounded-[4px] border-[#EC522E] border-2 bg-[#EC522E] text-[#fff]'>Submit</button>
+                        </div>
 
-                    {/* <button>Submit</button> */}
+                    </div>
                 </div>
-                {/* 
-                <div>
-                    <ul>
-                        <li>
-                            <a href="" className=''>Home</a>
-                        </li>
-                        <li>
-                            <a href="" className=''>Portfolio</a>
-                        </li>
-                        <li>
-                            <a href="" className=''>Product</a>
-                        </li>
-                        <li>
-                            <a href="" className=''>About Us</a>
-                        </li>
-                    </ul>
-                </div> */}
+
+            </div>
+
+            <div className='mt-20 text-[#fff] pt-5 flex justify-between'>
+                <ul className='flex gap-x-12 p-5 font-bold '>
+                    <li>
+                        <a href="" className=''>Home</a>
+                    </li>
+                    <li>
+                        <a href="" className=''>Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="" className=''>Product</a>
+                    </li>
+                    <li>
+                        <a href="" className=''>About Us</a>
+                    </li>
+                </ul>
+
+                <div className='mt-4'>
+                    <p>COPYRIGHT 2020 ALL RIGHT RESERVED</p>
+                </div>
             </div>
         </section>
     )
