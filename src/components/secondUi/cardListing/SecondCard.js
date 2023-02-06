8ed268e6814cd47f03904b771b85a303bde35c64
jennifer-pecky <ugwojennifer7@gmail.com>
2023-02-06 13:39:41 +0100

    completed

--- src/components/secondUi/cardListing/SecondCard.js
+++ src/components/secondUi/cardListing/SecondCard.js
@@ -24,14 +24,14 @@ const SecondCard = ({ imgUrl, title, desc, openSpots, rentSpots, prize, rating, 
                     <p className='font-bold text-base tracking-wide'>{title}</p>
                     <p className='text-[#EC522E] font-extrabold tracking-wide'>{prize}</p>
                     <p className='font-medium tracking-wide'>{desc}</p>
-                    <div className='grid grid-cols-3 space-x-3 relative'>
+                    <div className='grid grid-cols-3 relative'>
                         <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px]'>Watered</button>
                         <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold whitespace-nowrap hover:bg-[#707070] border-[#707070] w-[96px] h-[40px]'>Car pack</button>
                         <button className='mt-1 tracking-wide rounded-full text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px]'>Bar spot</button> <br />
-                        <button className='whitespace-nowrap tracking-wide rounded-full text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[144px] h-[40px] absolute -left-3 top-12'>Swimming pool</button>
+                        <button className='whitespace-nowrap tracking-wide rounded-full text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[144px] h-[40px] absolute top-12'>Swimming pool</button>
                     </div>
                     <div className='flex space-x-4 '>
-                        <div className='mt-9'>
+                        <div className='mt-8'>
                             <ReactStars
                                 count={5}
                                 onChange={ratingChanged}
@@ -40,12 +40,12 @@ const SecondCard = ({ imgUrl, title, desc, openSpots, rentSpots, prize, rating, 
 
                             />
                         </div>
-                        <p className='text-[#666666] font-[30px] text-[13px] mt-10'>{rating}</p>
-                        <h4 className='text-[#666666] font-[30px] text-[13px] mt-[3.0rem]'>{reviews}</h4>
+                        <p className='text-[#666666] font-[30px] text-[13px] mt-9'>{rating}</p>
+                        <h4 className='text-[#666666] font-[30px] text-[13px] mt-[2.8rem]'>{reviews}</h4>
                     </div>
                     <div className='flex space-x-3'>
-                        <button className='text-sm  font-semibold rounded-[6px] w-[95px] h-[42px] hover:bg-[#707070] text-[#EC522E] border-2 border-[#EC522E]'>VIEW</button>
-                        <button className='text-sm  font-semibold rounded-[6px] w-[200px] h-[42px] hover:bg-[#707070] text-[#fff] border-2 border-[#EC522E] bg-[#EC522E]'>CONTACT FOR INSPECTION</button>
+                        <button className='text-sm  font-semibold mt-2 rounded-[6px] w-[95px] h-[42px] hover:bg-[#707070] text-[#EC522E] border-2 border-[#EC522E]'>VIEW</button>
+                        <button className='text-sm  font-semibold mt-2 rounded-[6px] w-[200px] h-[42px] hover:bg-[#707070] text-[#fff] border-2 border-[#EC522E] bg-[#EC522E]'>CONTACT FOR INSPECTION</button>
                     </div>
 
                 </div>
