d6630feb8b7c3ffec1e20cea71d01a67b069120d
jennifer-pecky <ugwojennifer7@gmail.com>
2023-02-07 16:09:42 +0100

    still making chnages

--- src/components/About-section/About.js
+++ src/components/About-section/About.js
@@ -11,8 +11,9 @@ const About = () => {
                     <h1 className='text-[60px] font-black leading-tight whitespace-nowrap'>Who we are <br /> and what we do</h1>
                     <hr className="w-20 h-1 mt-4 bg-gray-100 border-0 rounded dark:bg-[#EC522E]"></hr>
                 </div>
-                <div className='text-[#666666] mt-6'>
-                    <p className='Text-[24px] font-medium whitespace-nowrap'>We are dedicated to helping our cilents buy, sell and rent properties. </p>
+                <div className='text-[#000000] mt-4'>
+                    <p className='Text-[24px] font-normal whitespace-nowrap'>We are dedicated to helping our cilents buy, sell and rent properties. <br /> Our team of experienced and professional agents is ready to assist <br /> you with all of your real estate needs. </p>
+                    <p className='Text-[24px] font-normal whitespace-nowrap mt-5'>We understand that buying or selling a home can be a stressful and <br /> overwhelming process, which is why we strive to make the experience <br /> as seamless and enjoyable as possible.</p>
                 </div>
             </div>
 
@@ -27,35 +28,8 @@ const About = () => {
                     <img src={img} alt="" className='w-[780px] h-[370px]' />
                 </div>
 
-                <div className='flex space-x-6'>
-                    <img src={img} alt="" className='w-[780px] h-[370px]' />
-                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
-                </div>
 
-                <div className='flex space-x-6'>
-                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
-                    <img src={img} alt="" className='w-[780px] h-[370px]' />
-                </div>
 
-                <div className='flex space-x-6'>
-                    <img src={img} alt="" className='w-[780px] h-[370px]' />
-                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
-                </div>
-
-                <div className='flex space-x-6'>
-                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
-                    <img src={img} alt="" className='w-[780px] h-[370px]' />
-                </div>
-
-                <div className='flex space-x-6'>
-                    <img src={img} alt="" className='w-[780px] h-[370px]' />
-                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
-                </div>
-
-                <div className='flex space-x-6'>
-                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
-                    <img src={img} alt="" className='w-[780px] h-[370px]' />
-                </div>
             </div>
         </section>
     )
