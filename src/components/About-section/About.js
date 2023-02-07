bfa5a5d5521681fb8d07cab893ea261884591ac7
jennifer-pecky <ugwojennifer7@gmail.com>
2023-02-07 14:26:14 +0100

    about commit

---
+++ src/components/About-section/About.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import img from '../../../Asset/images/img-17.jpg'
+import img2 from '../../../Asset/images/img-12.jpg'
+
+const About = () => {
+    return (
+        <section>
+            <div className='flex space-x-14 px-20'>
+                <div className='text-[#000000]'>
+                    <h1 className='text-[60px] font-black leading-tight whitespace-nowrap'>Welcome to our <br /> gallery & enjoy</h1>
+                    <hr className="w-20 h-1 mt-4 bg-gray-100 border-0 rounded dark:bg-[#EC522E]"></hr>
+                </div>
+                <div className='text-[#666666] mt-6'>
+                    <p className='Text-[24px] font-medium whitespace-nowrap'>Our gallery only contains properties that has already been sold, rented <br /> leased.</p>
+                </div>
+            </div>
+
+            <div className='flex flex-col px-20 space-y-6 mt-6'>
+                <div className='flex space-x-6'>
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                </div>
+
+                <div className='flex space-x-6'>
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                </div>
+
+                <div className='flex space-x-6'>
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                </div>
+
+                <div className='flex space-x-6'>
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                </div>
+
+                <div className='flex space-x-6'>
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                </div>
+
+                <div className='flex space-x-6'>
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                </div>
+
+                <div className='flex space-x-6'>
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                </div>
+
+                <div className='flex space-x-6'>
+                    <img src={img2} alt="" className='w-[361px] h-[370px]' />
+                    <img src={img} alt="" className='w-[780px] h-[370px]' />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import About from '../components/About-section/About'
 
 const AboutUs = () => {
     return (
-        <div>AboutUs</div>
+        <div>
+            <About />
+        </div>
     )
 }
 
