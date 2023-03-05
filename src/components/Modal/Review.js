0796291e7a77d0553676fdd8e93026f061557609
jennifer-pecky <ugwojennifer7@gmail.com>
2023-03-05 15:16:26 +0100

    worked on review modal

--- src/components/Modal/Review.js
+++ src/components/Modal/Review.js
@@ -39,7 +39,7 @@ const Review = () => {
                                     activeColor="#ffd700"
                                 />
                             </div>
-                            <p className='Text-[30px] font-normal leading-relaxed'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et  dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo  dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem</p>
+                            <p className='Text-[30px] font-normal leading-relaxed'>Lorem ipsum color sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et  dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo  dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem</p>
                         </div>
                     </div>
 
