dc85102e0b432a0d1657e4ffb109ca0508e3c3b4
jennifer-pecky <ugojennifer7@gmail.com>
2024-01-02 21:57:43 +0100

    adding responsiveness

--- src/components/HomeModal/HomeModal.js
+++ src/components/HomeModal/HomeModal.js
@@ -1,50 +1,76 @@
-import React, { useContext } from 'react'
-import ReviewHome from './ReviewHome'
-import img from '../../Asset/images/img-4.jpg'
-import './HomeStyle.css'
-import { ModalContext } from '../../Context/Modalcontext'
+import React, { useContext } from "react";
+import ReviewHome from "./ReviewHome";
+import img from "../../Asset/images/img-4.jpg";
+import "./HomeStyle.css";
+import { ModalContext } from "../../Context/Modalcontext";
 
 const HomeModal = () => {
-    const { setHomeModal } = useContext(ModalContext)
+  const { setHomeModal } = useContext(ModalContext);
 
-    const handleModalClose = () => {
-        setHomeModal(false)
-    }
-    return (
-        <section className='absolute z-20' onClick={handleModalClose}>
-            <div className='flex'>
-                <div className='w-[630px] h-[650px] bg-[#00000040]'></div>
-                <div className='max-w-full w-[683px] bg-[#fff] max-h-[600px] overflow-y-auto scroll pop-up'>
-                    <div className='relative'>
-                        <img src={img} alt="" className='w-[683px] h-[377px] max-w-full' />
-                        <div className='absolute left-5 bg-[#fff] p-2 rounded-[20px] pl-5 font-bold bottom-[20rem] w-[7rem]'>Availble</div>
-                        <div className='absolute right-7 bg-[#fff] p-[0.6rem] pl-5 rounded-[30px] w-[5rem] h-[2.6rem] font-bold bottom-[20rem]'>Sale</div>
-                    </div>
-                    <div className='p-2 space-y-3'>
-                        <div className='flex justify-between'>
-                            <h1 className='font-bold text-base tracking-wide'>4 Bedroom Duplex at Odili Road</h1>
-                            <span className='text-[#EC522E] font-extrabold tracking-wide'>N50,000,000</span>
-                        </div>
-                        <h3 className='font-medium tracking-wide'>Odii Road, Port Harcourt, River State</h3>
-                        <p className='Text-[30px] font-normal leading-relaxed'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem</p>
-
-                        <div className='flex space-x-2'>
-                            <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px]'>Watered</button>
-                            <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px] whitespace-nowrap'>Car pack</button>
-                            <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px] whitespace-nowrap'>Bar spot</button>
-                            <button className='rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[144px] h-[40px] whitespace-nowrap'>Swimming pool</button>
-                        </div>
-                    </div>
-
-                    <hr />
-                    <ReviewHome />
-                    <br />
-
-
-                </div>
+  const handleModalClose = () => {
+    setHomeModal(false);
+  };
+  return (
+    <section className="absolute z-20" onClick={handleModalClose}>
+      <div className="flex">
+        <div className="w-[630px] h-[650px] bg-[#00000040]"></div>
+        <div className="max-w-full w-[683px] bg-[#fff] max-h-[600px] overflow-y-auto scroll pop-up">
+          <div className="relative">
+            <img src={img} alt="" className="w-[683px] h-[377px] max-w-full" />
+            <div className="absolute left-5 bg-[#fff] p-2 rounded-[20px] pl-5 font-bold bottom-[20rem] w-[7rem]">
+              Availble
             </div>
-        </section>
-    )
-}
+            <div className="absolute right-7 bg-[#fff] p-[0.6rem] pl-5 rounded-[30px] w-[5rem] h-[2.6rem] font-bold bottom-[20rem]">
+              Sale
+            </div>
+          </div>
+          <div className="p-2 space-y-3">
+            <div className="flex justify-between">
+              <h1 className="font-bold text-base tracking-wide">
+                4 Bedroom Duplex at Odili Road
+              </h1>
+              <span className="text-[#EC522E] font-extrabold tracking-wide">
+                N50,000,000
+              </span>
+            </div>
+            <h3 className="font-medium tracking-wide">
+              Odii Road, Port Harcourt, River State
+            </h3>
+            <p className="Text-[30px] font-normal leading-relaxed">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+            </p>
 
-export default HomeModal
+            <div className="flex space-x-2">
+              <button className="rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px]">
+                Watered
+              </button>
+              <button className="rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px] whitespace-nowrap">
+                Car pack
+              </button>
+              <button className="rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[96px] h-[40px] whitespace-nowrap">
+                Bar spot
+              </button>
+              <button className="rounded-full tracking-wide text-xs border-[1px] font-semibold hover:bg-[#707070] border-[#707070] w-[144px] h-[40px] whitespace-nowrap">
+                Swimming pool
+              </button>
+            </div>
+          </div>
+
+          <hr />
+          <ReviewHome />
+          <br />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeModal;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,83 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../Asset/images/house.png";
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="hidden md:block">
+      <div className="flex md:space-x-24 md:px-40 p-6 justify-center">
+        <div className="pt-20">
+          <div className="text-center space-y-10 ">
+            <div>
+              <h1 className="text-neutral-900 text-3xl font-bold font-['Roboto Serif']">
+                Sign in to your account
+              </h1>
+              <h3 className="text-neutral-500 text-xl font-medium font-['Poppins']">
+                Welcome back!
+              </h3>
+            </div>
+            <div className="space-y-4">
+              <div className="inline-flex items-center justify-center space-x-5 w-full ">
+                <hr className="w-60 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+                <span className="">or</span>
+                <hr className="w-60 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+              </div>
+              <form action="">
+                <div className="space-y-4">
+                  <div className="relative">
+                    <input
+                      type="text"
+                      className="border border-neutral-400 text-[#A1A1A1] text-sm rounded  block w-full ps-10 p-2.5"
+                      placeholder="Email address"
+                    />
+                  </div>
+                  <div className="relative">
+                    <input
+                      type="password"
+                      className="border border-neutral-400 text-[#A1A1A1] text-sm rounded  block w-full ps-10 p-2.5"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex space-x-4">
+                      <input
+                        type="checkbox"
+                        name="Remember me"
+                        id=""
+                        className="w-6 h-6 bg-white rounded border border-stone-300"
+                      />
+                      <p className="text-zinc-500 text-base font-medium font-['Poppins']">
+                        Remember me
+                      </p>
+                    </div>
+                    <Link
+                      href={"/ResetPassword"}
+                      className="text-red-400 text-base font-bold font-['Poppins']"
+                    >
+                      Forgotten password?
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="space-y-7 pt-16">
+                  <div className=" px-72 py-4 bg-red-400 rounded justify-center items-center inline-flex mt-10">
+                    <Link href={"/dashboard"}>
+                      <button className="text-white text-base font-bold font-['Poppins'] whitespace-nowrap">
+                        Sign up
+                      </button>
+                    </Link>
+                  </div>
+                  <div className="flex justify-center items-center space-x-3">
+                    <p>Don`t have an account?</p>
+                    <Link className="text-red-400" href={"/signup"}>
+                      Create an account
+                    </Link>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
